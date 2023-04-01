56a12ea30ebd66eace1999aadaac24e15bfc81e6
renan rezende <renanrezende224@gmail.com>
2023-03-31 23:45:29 -0300

    material ui icons/db

--- src/App.js
+++ src/App.js
@@ -6,10 +6,27 @@ function App() {
   return (
     <div className="App">
       <div className='app__videos'>
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+        <Video 
+        
+        likes={111}
+        messages={222}
+        shares={333}
+        name="Renan"
+        description="Gato fofinho"
+        music="musica "
+        url="https://privaty.com.br/wp-content/uploads/2023/03/gato-1.mp4"
+        />
+        <Video 
+        
+        likes={444}
+        messages={522}
+        shares={333}
+        name="Rafael"
+        description="Gato brabo"
+        music="Your hands"
+        url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4"
+        
+      />
 
       </div>
  
